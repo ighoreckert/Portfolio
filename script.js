adc132e10e21ed8516b2c081247b092db612119f
Ighor Eckert <ighor.eckert@gmail.com>
2022-03-30 02:03:37 -0300

    :package: bugdet calc

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ document
   .addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
   );
+
+document.querySelector("#qnty").addEventListener("change", updatePrice);
+document.querySelector("#js").addEventListener("change", updatePrice);
+document.querySelector("#layout-yes").addEventListener("change", updatePrice);
+document.querySelector("#layout-no").addEventListener("change", updatePrice);
+document.querySelector("#deadline").addEventListener("change", function () {
+  const deadLine = document.querySelector("#deadline").value;
+  document.querySelector(
+    "label[for=deadline]"
+  ).innerHTML = `Deadline: ${deadLine} weeks`;
+  updatePrice();
+});
+
+function updatePrice() {
+  const qnty = document.querySelector("#qnty").value;
+  const hasJS = document.querySelector("#js").checked;
+  const layoutYes = document.querySelector("#layout-yes").checked;
+  const deadLine = document.querySelector("#deadline").value;
+  let price = qnty * 100;
+  if (hasJS) price *= 1.1;
+  if (layoutYes) price += 500;
+  let deadlineUrgentTax = 1 - deadLine * 0.1;
+  price *= 1 + deadlineUrgentTax;
+
+  document.querySelector("#price").innerHTML = `R$ ${price.toFixed(2)}`;
+}
